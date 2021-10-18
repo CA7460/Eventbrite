8d6f19b52fa8fee8102853d715b64703225bfc05
OnceNamedSam <202195592@collegeahuntsic.qc.ca>
2021-10-18 16:04:11 -0400

    main ready

--- event_app/lib/main.dart
+++ event_app/lib/main.dart
@@ -4,11 +4,29 @@ void main() {
   runApp(const MyApp());
 }
 
+Widget homePage() {
+//  return const Text('test');
+
+// Si le user est déja inscrit, direction vers la liste 'devents (module event_manager)
+// si l'app est ouverte pour la première fois, direction login form (module login)
+
+// Module login à élaborer dans les dossiers appropriés 
+// Ne pas oublier de l'importer ici 
+  return loginScreen();
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: homePage(),
+        ),
+      ),
+    );
   }
 }
